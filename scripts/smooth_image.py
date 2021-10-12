49f34c3e0aa5abba4f696be7496ab461407c54bd
Jiachen Feng <snidero.feng21@imperial.ac.uk>
2021-10-12 22:56:43 +0100

    <my commit message>

--- scripts/smooth_image.py
+++ scripts/smooth_image.py
@@ -1,5 +1,23 @@
-from envtest import rand_array
+# from envtest import rand_array
+#
+# shape = (3, 3)
+#
+# print(rand_array(shape))
 
-shape = (3, 3)
+from envtest import smooth_image
 
-print(rand_array(shape))
+from scipy import misc
+import matplotlib.pyplot as plt
+
+
+image = misc.ascent()
+sigma = 5
+
+smoothed_image = smooth_image(image, sigma)
+
+f = plt.figure()
+f.add_subplot(1, 2, 1)
+plt.imshow(image)
+f.add_subplot(1, 2, 2)
+plt.imshow(smoothed_image)
+plt.show(block=True)
